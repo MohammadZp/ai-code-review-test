a0d992abb8df76dac69e2f88acff1227b5447596
Mohammad zahedipour <zahedipourmohammad@gmail.com>
2025-05-06 19:01:56 +0330

    test for ci cd

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
-def calculate_area(radius):
-    # This function calculates the area of a circle
-    pi = 3.14159  # Approximation of pi
-    area = pi * radius ** 2
-    return area
-
-def calculate_perimeter(radius):
+def calculate_perimeter(a):
     # This function calculates the perimeter (circumference) of a circle
     pi = 3.14159  # Approximation of pi
-    perimeter = 2 * pi * radius
+    perimeter = 2 * pi * a
     return perimeter
 
 def main():
@@ -16,7 +10,20 @@ def main():
     perimeter = calculate_perimeter(radius)
     
     print(f"The area of the circle with radius {radius} is: {area}")
-    print(f"The perimeter of the circle with radius {radius} is: {perimeter}")
+    print(f"The perimeter of the circle with radius {radius} is: {perimeter}") 
+
+
+
+def calculate_area(radius):
+    # This function calculates the area of a circle
+    pi = 3.14159  # Approximation of pi
+    area = pi * radius ** 2
+    return area
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
